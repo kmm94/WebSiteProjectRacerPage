98e0dd9a60ad264a1715ac9b30ce566afbfa3c2e
karim møller <karimkuku@gmail.com>
2018-05-12 18:16:57 +0200

    add html former

--- WebSiteProjectRacerPage/Pages/AboutMe.cshtml.cs
+++ WebSiteProjectRacerPage/Pages/AboutMe.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
